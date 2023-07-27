725f628d9741ae62accbf5848bed542f8e69f7a7
john <johnthomas222000@gmail.com>
2023-07-27 22:25:26 +0530

    added emoji reactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import AddPostForm from "./features/post/AddPostForm";
 import PostList from "./features/post/PostsList";
 function App() {
   return (
     <main>
+      <AddPostForm />
       <PostList />
     </main>
   );
--- src/features/post/PostsSlice.ts
+++ src/features/post/PostsSlice.ts
@@ -1,30 +1,74 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
+import { nanoid } from "@reduxjs/toolkit";
 const initialState = [
   {
     id: "1",
-    title: "Learning Redux Toolit",
-    content: "Ive heared good things.",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: "2",
-    title: "Slices",
-    content: "The more i say slice, the more I want pizza",
+    title: "Slices...",
+    content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
-const postSlice = createSlice({
+const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload);
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
-export default postSlice.reducer;
+export default postsSlice.reducer;
